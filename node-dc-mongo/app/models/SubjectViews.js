e9acc10e801fa52d67627296b48f650d40e664f6
NickLacunha <njpadinha@gmail.com>
2017-03-19 18:01:04 -0700

    Update models and routes
    API should now serve the data from Salaries.csv

--- node-dc-mongo/app/models/SubjectViews.js
+++ node-dc-mongo/app/models/SubjectViews.js
@@ -1,4 +1,6 @@
 
-// define our nerd model
+// define our salaries model data-first
 // module.exports allows us to pass this to other files when it is called
-module.exports = connectionsubject.model('', {}, 'projects');
+// this code must be run after connectionsubject has been initialized
+// I think this is a weakness that should be refactored, personally.
+module.exports = connectionsubject.model('', {}, 'salaries');
